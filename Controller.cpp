40dd81631757945d8b077f3b82462d6480e20607
Adam Weizman <adamweizman@gmail.com>
2018-02-20 11:46:45 -0500

    Controller.cpp file
    Added structure for Controller, including variables for tempo, playing, resetting playback, looping track as well as functions to get and set these variables.

---
+++ Controller.cpp
@@ -0,0 +1,35 @@
+//Added by Adam Weizman, 2/20/2018
+class Controller {
+public:
+	void resetSetter() { //sets sets playback to beginning of track
+		
+	}
+
+	void tempoSetter(int tempo) { //sets tempo via user input
+		this.tempo = tempo;
+	}
+
+	int tempoGetter() { //returns tempo in bpm
+		return tempo;
+	}
+
+	void playSetter() { //sets play to be true or false
+		if (play) {
+			play = false;
+			
+		}
+		else {
+			play = true;
+		}
+		
+	}
+	bool playGetter() { //returns status of play variable
+		return play;
+	}
+
+private:
+	bool play = false; //if play is true, track is playing. if false, stops at current position.
+	int tempo = 100; //determines speed of playback in bpm. If stopped, tempo retains its value for future playback. 100 bpm is standard, used in CPR and "Staying Alive"
+	bool loop = false: //if loop is true, when the end is reached, will start from the beginning, allowing program to work as a beatmixer or a song-maker. achieved w/ do-while loop
+};
+//Added by Adam Weizman, 2/20/2018
