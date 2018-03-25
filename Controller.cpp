046c765bcef80e1991ce9fc9b95c499650dcd764
Adam Weizman <adamweizman@gmail.com>
2018-03-25 18:52:22 -0400

    Added new variables and commens according to Design doc in google Drive

--- Controller.cpp
+++ Controller.cpp
@@ -1,8 +1,9 @@
 //Added by Adam Weizman, 2/20/2018
 class Controller {
 public:
-	void resetSetter() { //sets sets playback to beginning of track
-		
+
+	void playTrack() { //checks value of playing variable when play button is clicked in GUI. Accessed by main fxn.
+
 	}
 
 	void tempoSetter(int tempo) { //sets tempo via user input
@@ -13,22 +14,24 @@ public:
 		return tempo;
 	}
 
-	void playSetter() { //sets play to be true or false
-		if (play) {
-			play = false;
+	void playingSetter() { //sets play to be true or false. Accessed by GUI
+		if (playing) {
+			playing = false;
 			
 		}
 		else {
-			play = true;
+			playing = true;
 		}
 		
 	}
-	bool playGetter() { //returns status of play variable
+	bool playingGetter() { //returns status of play variable
 		return play;
 	}
 
 private:
-	bool play = false; //if play is true, track is playing. if false, stops at current position.
+	const bool[][] track;
+	int playheadIndex; //
+	bool playing = false; //if play is true, track is playing. if false, stops at current position.
 	int tempo = 100; //determines speed of playback in bpm. If stopped, tempo retains its value for future playback. 100 bpm is standard, used in CPR and "Staying Alive"
 };
 //Added by Adam Weizman, 2/20/2018
